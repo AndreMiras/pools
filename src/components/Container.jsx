ae825bf1af69d1048832e89418cef484808f2698
Andre Miras <andre.miras@gmail.com>
2020-10-15 22:05:45 +0200

    :construction: Gives react-bootstrap a try

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Container as ReactContainer } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const Container = () => {
   return (
-    <div className="container">
+    <ReactContainer>
       <h2>Uniswap ROI</h2>
-    </div>
+    </ReactContainer>
   );
 };
 
