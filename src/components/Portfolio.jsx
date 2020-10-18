997e8405b980199dc61461a012b90aecef26ee99
Andre Miras <andre.miras@gmail.com>
2020-10-18 18:13:21 +0200

    :sparkles: Adds portfolio share chart

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -2,10 +2,36 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import 'startbootstrap-sb-admin-2/css/sb-admin-2.min.css';
 import { Accordion, Card, Button } from 'react-bootstrap';
+import { Doughnut } from 'react-chartjs-2';
 import Pairs from './Pairs';
 
 const PortfolioPropTypes = {
   dataDict: PropTypes.shape({
+    address: PropTypes.string.isRequired,
+    pairs: Pairs.isRequired,
+  }).isRequired,
+};
+
+const DoughnutChart = ({ dataDict }) => {
+  const balance = dataDict.balance_usd;
+  const datasetsData = dataDict.pairs.map(
+    (pairDict) => Math.round(100 * pairDict.balance_usd / balance),
+  );
+  const labels = dataDict.pairs.map(
+    (pairDict) => pairDict.pair_symbol,
+  );
+  const data = {
+    labels,
+    datasets: [{
+      label: 'Portfolio share',
+      data: datasetsData,
+    }],
+  };
+  return <Doughnut data={data} />;
+};
+DoughnutChart.propTypes = {
+  dataDict: PropTypes.shape({
+    balance_usd: PropTypes.number.isRequired,
     address: PropTypes.string.isRequired,
     pairs: Pairs.isRequired,
   }).isRequired,
@@ -33,6 +59,7 @@ DebugResponse.propTypes = PortfolioPropTypes;
 
 const Portfolio = ({ dataDict }) => (
   <div>
+    <DoughnutChart dataDict={dataDict} />
     <Pairs address={dataDict.address} pairs={dataDict.pairs} />
     <DebugResponse dataDict={dataDict} />
   </div>
