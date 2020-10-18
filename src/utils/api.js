b572660db4b42e83f31e761480ad400b5537c882
Andre Miras <andre.miras@gmail.com>
2020-10-18 17:24:19 +0200

    :goal_net: Handles unhandled backend errors

--- src/utils/api.js
+++ src/utils/api.js
@@ -10,7 +10,12 @@ const fetchPortfolio = (address, onOk, onNotOk) => {
     .then((response) => {
       const callback = response.ok ? onOk : onNotOk;
       response.json().then((data) => callback(data));
-    });
+    },
+    // e.g. on unhandled backend 500 exception, the cors header is not passed
+    // hence the frontend can't even fetch the response, error would be:
+    // TypeError: Failed to fetch
+    // TODO: we may want to cross log that to Sentry
+    (error) => onNotOk({ detail: error.toString() }));
 };
 
 export { getPortfolioUrl, fetchPortfolio };
